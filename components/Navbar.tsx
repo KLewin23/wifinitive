1157f1ac12abc9b1262c56a6ab0f3fed3ee60eff
Kieran Lewin <afrenchrussian@gmail.com>
2022-01-10 04:21:09 +0000

    removed lint issues

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Logo from './Logo';
 import styles from '../styles/Components.module.css';
+import useWindowWidth from './useWindowWidth';
 
 const Navbar = ({ mode }: { mode: 'light' | 'dark' }): JSX.Element => {
+    const windowWidth = useWindowWidth();
+
     return (
         <div
             className={styles.navbar}
@@ -11,23 +14,31 @@ const Navbar = ({ mode }: { mode: 'light' | 'dark' }): JSX.Element => {
             }}
         >
             <Logo mode={mode === 'light' ? 'dark' : 'light'} />
-            <div className={styles.links}>
-                <a href={'/'}>
-                    <h4>Home</h4>
-                </a>
-                <a href={'/products'}>
-                    <h4>Products</h4>
-                </a>
-                <a href={'/services'}>
-                    <h4>Services</h4>
-                </a>
-                <a href={'/contact'}>
-                    <h4>Contact</h4>
-                </a>
-                <a href={'/about'}>
-                    <h4>About</h4>
-                </a>
-            </div>
+            {windowWidth > 850 ? (
+                <div className={styles.links}>
+                    <a href={'/'}>
+                        <h4>Home</h4>
+                    </a>
+                    <a href={'/products'}>
+                        <h4>Products</h4>
+                    </a>
+                    <a href={'/services'}>
+                        <h4>Services</h4>
+                    </a>
+                    <a href={'/contact'}>
+                        <h4>Contact</h4>
+                    </a>
+                    <a href={'/about'}>
+                        <h4>About</h4>
+                    </a>
+                </div>
+            ) : (
+                <div>
+                    <span />
+                    <span />
+                    <span />
+                </div>
+            )}
         </div>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
@@ -6,10 +6,11 @@ import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
 const Home: NextPage = () => {
-    const [hoveredTile, setHoveredTile] = useState<null | number>(null);
+    const [hoveredTile, setHoveredTile] = useState<null | number>(0);
+    const quarterTiles = useRef<HTMLDivElement | null>(null);
 
     return (
-        <div className={styles.container}>
+        <>
             <Head>
                 <title>Wifinitive</title>
                 <meta name={'description'} content={'Wifinitive website - homepage'} />
@@ -17,21 +18,24 @@ const Home: NextPage = () => {
             </Head>
             <main>
                 <Navbar mode={'dark'} />
-                <div className={styles.quarterTiles}>
+                <div className={styles.quarterTiles} ref={quarterTiles}>
                     <div className={styles.tile} onMouseEnter={() => setHoveredTile(0)} onMouseLeave={() => setHoveredTile(null)}>
                         <h2>Decahedra</h2>
                         <p>Delivering Scalable and Secure Data Centre Solutions Across Europe, Asia and Africa.</p>
                         <a href={'/decahedra'}>Learn more</a>
+                        <div className={styles.slide1} style={{ transform: hoveredTile === 0 ? `rotate(-36deg) scaleX(2.7)` : `rotate(-36deg)` }} />
                     </div>
                     <div className={styles.tile} onMouseEnter={() => setHoveredTile(1)} onMouseLeave={() => setHoveredTile(null)}>
                         <h2>Consultancy</h2>
                         <p>Utilise our world-class team for your next project and benefit from lower costs and full support.</p>
                         <a href={'/consultancy'}>Learn more</a>
+                        <div className={styles.slide2} style={{ transform: hoveredTile === 1 ? `rotate(-36deg) scaleX(2.7)` : `rotate(-36deg)` }} />
                     </div>
                     <div className={styles.tile} onMouseEnter={() => setHoveredTile(2)} onMouseLeave={() => setHoveredTile(null)}>
                         <h2>IT Management</h2>
                         <p>On Premises or Remote IT Contracting for your platforms and services.</p>
                         <a href={'/it-management'}>Learn more</a>
+                        <div className={styles.slide3} style={{ transform: hoveredTile === 2 ? 'rotate(-36deg) scaleX(3.3)' : 'rotate(-36deg)' }} />
                     </div>
                     <div className={styles.tile} onMouseEnter={() => setHoveredTile(3)} onMouseLeave={() => setHoveredTile(null)}>
                         <h2>Decahedra Cloud</h2>
@@ -40,18 +44,13 @@ const Home: NextPage = () => {
                             partner for your future.
                         </p>
                         <a href={'/decahedra-cloud'}>Learn more</a>
-                    </div>
-                    <div className={styles.sliders}>
-                        <div className={styles.slide1} style={{ transform: hoveredTile === 0 ? 'rotate(-32deg) scaleX(2.7)' : 'rotate(-32deg)' }} />
-                        <div className={styles.slide2} style={{ transform: hoveredTile === 1 ? 'rotate(-32deg) scaleX(2.85)' : 'rotate(-32deg)' }} />
-                        <div className={styles.slide3} style={{ transform: hoveredTile === 2 ? 'rotate(-32deg) scaleX(3.6)' : 'rotate(-32deg)' }} />
-                        <div className={styles.slide4} style={{ transform: hoveredTile === 3 ? 'rotate(-32deg) scaleX(3.6)' : 'rotate(-32deg)' }} />
+                        <div className={styles.slide4} style={{ transform: hoveredTile === 3 ? 'rotate(-36deg) scaleX(3.3)' : 'rotate(-36deg)' }} />
                     </div>
                 </div>
                 <div className={styles.stats}>stats</div>
             </main>
             <Footer />
-        </div>
+        </>
     );
 };
 
